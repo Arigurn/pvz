18b95ffd3df020a3c07b5e37a9e7f5e935bfc636
Arigurn <anton.tupicin@gmail.com>
2020-05-09 17:46:47 +0300

    2020.05.09 17:46

--- index.php
+++ index.php
@@ -31,22 +31,15 @@ function dbcreatefield( $post ) {
 if ($_GET['work'] > 0)
 if ( isset( $_POST ) ) {
     $json = file_get_contents('php://input');
-    //echo var_dump($json);
     $msg = json_decode($json, true);
-    //echo var_dump($msg);
-    $id = $msg['id'];
-    $id = $id+0;
-    //echo var_dump($id);
     require( "php/rb-mysql.php" );
     R::setup( 'mysql:host=localhost;dbname=Role-play', 'php', '157855' );
     if (!R::testConnection()) {
 		echo "<br/>Не удалось подключиться к БД";
 		exit;
 	}
-    $_SESSION[ 'player' ] = R::load( 'players', $id );
-    R::close();
-    echo json_encode( $_SESSION[ 'player' ] );
-    
+    $msg = json_decode($json, true);
+    dbcreatefield( $msg );
 }
 if ($_GET['demo'] == 1) {
     $json = file_get_contents('./test.json', true);
